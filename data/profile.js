997a9e166eaf3d32dc98bdbc77ea6a05a882ee2e
Diego Uribe <duribes89@hotmail.com>
2026-03-17 12:33:54 -0500

    Update profile.js

--- data/profile.js
+++ data/profile.js
@@ -12,6 +12,7 @@ const PROFILE = {
   office: "Office ML - 622",
   photo: "assets/diego_uribe.jpg", // Reemplaza con tu foto
   bio: `Diego Uribe-Suárez is a postdoctoral researcher in the Department of Mechanical Engineering at Universidad de los Andes, where he also teaches undergraduate and graduate courses. He earned his B.Sc. in Mechanical Engineering and his M.Sc. in Engineering from Universidad EAFIT in Medellín, Colombia. He received his Ph.D. in Computational Mechanics and Materials from Université Côte d’Azur, France, through a collaboration between the Centre for Material Forming (CEMEF MINES ParisTech) and the Observatoire de la Côte d’Azur (OCA–Université Côte d’Azur).
+
 His research focuses on the development and implementation of advanced numerical methods for the simulation of complex engineering problems, with applications in the mechanical behavior of materials, fracture and damage mechanics, and structural analysis. Currently, he works on the Material Point Method (MPM) and the Virtual Element Method (VEM), exploring their potential for modeling challenging problems in mechanics and materials science. His background combines strong expertise in finite element analysis, numerical simulation, and applied mathematics with hands-on experience in scientific software development. He is passionate about advancing computational mechanics and aims to contribute to both the theoretical foundations and practical applications of modern simulation tools while fostering collaboration between academia and industry.
 `,
   social: {
