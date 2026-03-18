b8303dc80c232eda7e93d52c6e67e114c7a5dd07
María Alejandra Vargas Torres <69637081+maleja2309@users.noreply.github.com>
2026-03-18 18:38:48 -0500

    Update group.js

--- data/group.js
+++ data/group.js
@@ -17,7 +17,7 @@ var GROUP_MEMBERS = [
   {
     id: 1,
     name: "Dr. Fernando Ramirez Rodriguez",
-    position: "Profesor Titular",
+    position: "Full Professor",
     formation: "MSc and PhD degrees from Colorado State University.",
     description: "Dr. Fernando Ramirez is a Civil Engineer whose main area of expertise is Theoretical and Computational Mechanics. He earned his MSc and PhD degrees from Colorado State University. Dr Ramirez is currently a full professor at Universidad de los Andes in Colombia. His main topics of research include theoretical and computational mechanics, nanomechanics and biomechanics, structural mechanics, and composite and smart materials, among others. Dr. Ramirez has more than 250 publications, including over 35 highly cited peer reviewed journal papers. Dr. Ramirez also has two international patents related to his biomechanics research.",
     email: "framirez@uniandes.edu.co",
