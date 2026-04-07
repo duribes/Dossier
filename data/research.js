959c5f4c750f31bd0197baaf8ab89b2cdce17888
Diego Uribe <duribes89@hotmail.com>
2026-04-07 09:43:29 -0500

    Update research.js

--- data/research.js
+++ data/research.js
@@ -2,155 +2,148 @@ var RESEARCH_TOPICS = [];
 
 var PUBLICATIONS_BIBTEX = `
 @article{LuisAmigo_2023,
-  title = {Analysis of the added value for the quantitative reasoning competency at the Luis Amigó Catholic University in 2021},
-  journal = {Revista de Investigación, Desarrollo e Innovación},
-  volume = {13},
-  issue = {2},
-  pages = {329-344},
-  year = {2023},
-  issn = {2027-8306},
-  doi = {https://doi.org/10.19053/20278306.v13.n2.2023.16838},
-  author = {G.J. Posada-Hernández and M. López-Bonilla and D.A. Uribe-Suárez and V and L.F. Cardona-Palacio}
+title = {Analysis of the added value for the quantitative reasoning competency at the Luis Amigó Catholic University in 2021},
+journal = {Revista de Investigación, Desarrollo e Innovación},
+volume = {13},
+issue = {2},
+pages = {329-344},
+year = {2023},
+issn = {2027-8306},
+doi = {https://doi.org/10.19053/20278306.v13.n2.2023.16838},
+author = {G.J. Posada-Hernández and M. López-Bonilla and D.A. Uribe-Suárez and V and L.F. Cardona-Palacio}
 }
 
 @article{LuisAmigo_2024,
-  title = {Estimating the added value of critical reading competence in college students using statistical modeling},
-  journal = {Revista de Investigación, Desarrollo e Innovación},
-  volume = {15},
-  year = {2025},
-  issn = {2027-8306},
-  month = {06},
-  pages = {67-86},
-  doi = {https://doi.org/10.19053/uptc.20278306.v15.n1.2025.18823},
-  author = {G.J. Posada-Hernández and M. López-Bonilla and D.A. Uribe-Suárez and V. Gómez-Ceballos and L.F. Cardona-Palacio}
+title = {Estimating the added value of critical reading competence in college students using statistical modeling},
+journal = {Revista de Investigación, Desarrollo e Innovación},
+volume = {15},
+year = {2025},
+issn = {2027-8306},
+month = {06},
+pages = {67-86},
+doi = {https://doi.org/10.19053/uptc.20278306.v15.n1.2025.18823},
+author = {G.J. Posada-Hernández and M. López-Bonilla and D.A. Uribe-Suárez and V. Gómez-Ceballos and L.F. Cardona-Palacio}
 }
 
 @article{Uribe2020,
-  title = {Numerical modeling of crack propagation with dynamic insertion of cohesive elements},
-  journal = {Engineering Fracture Mechanics},
-  volume = {227},
-  pages = {106918},
-  year = {2020},
-  issn = {0013-7944},
-  doi = {https://doi.org/10.1016/j.engfracmech.2020.106918},
-  url = {http://www.sciencedirect.com/science/article/pii/S0013794419309324},
-  author = {D. Uribe-Suárez and P.-O. Bouchard and M. Delbo and D. Pino-Muñoz}
+title = {Numerical modeling of crack propagation with dynamic insertion of cohesive elements},
+journal = {Engineering Fracture Mechanics},
+volume = {227},
+pages = {106918},
+year = {2020},
+issn = {0013-7944},
+doi = {https://doi.org/10.1016/j.engfracmech.2020.106918},
+url = {http://www.sciencedirect.com/science/article/pii/S0013794419309324},
+author = {D. Uribe-Suárez and P.-O. Bouchard and M. Delbo and D. Pino-Muñoz}
 }
 
 @article{UribeApplicationComets2020,
-  title = {Diurnal temperature variation as the source of the preferential direction of fractures on asteroids: Theoretical model for the case of Bennu},
-  journal = {Icarus},
-  volume = {360},
-  pages = {114347},
-  year = {2021},
-  issn = {0019-1035},
-  doi = {https://doi.org/10.1016/j.icarus.2021.114347},
-  url = {https://www.sciencedirect.com/science/article/pii/S0019103521000440},
-  author = {D. Uribe-Suárez and M. Delbo and P.-O. Bouchard and D. Pino-Muñoz}
+title = {Diurnal temperature variation as the source of the preferential direction of fractures on asteroids: Theoretical model for the case of Bennu},
+journal = {Icarus},
+volume = {360},
+pages = {114347},
+year = {2021},
+issn = {0019-1035},
+doi = {https://doi.org/10.1016/j.icarus.2021.114347},
+url = {https://www.sciencedirect.com/science/article/pii/S0019103521000440},
+author = {D. Uribe-Suárez and M. Delbo and P.-O. Bouchard and D. Pino-Muñoz}
 }
 
 @article{UTSA2017,
-  title = {A multi-scale approach to describe electrical impulses propagating along actin filaments in both intracellular and in vitro conditions},
-  author = {Christian Hunley and D.A. Uribe Suárez and Marcelo Marucho},
-  journal = {RSC Adv.},
-  year = {2018},
-  volume = {8},
-  issue = {22},
-  pages = {12017-12028},
-  publisher = {The Royal Society of Chemistry},
-  doi = {10.1039/C7RA12799E},
-  url = {http://dx.doi.org/10.1039/C7RA12799E}
+title = {A multi-scale approach to describe electrical impulses propagating along actin filaments in both intracellular and in vitro conditions},
+author = {Christian Hunley and D.A. Uribe Suárez and Marcelo Marucho},
+journal = {RSC Adv.},
+year = {2018},
+volume = {8},
+issue = {22},
+pages = {12017-12028},
+publisher = {The Royal Society of Chemistry},
+doi = {10.1039/C7RA12799E},
+url = {http://dx.doi.org/10.1039/C7RA12799E}
 }
 
 @article{CIOH20151,
-  title = {Modelamiento matemático de la dinámica marina en la región de Cartagena},
-  author = {D.A. Uribe Suárez and Escobar Sierra, C. A. and Ruiz Sarrázola, D. A.},
-  year = {2015},
-  issue = {33},
-  pages = {69-86},
-  doi = {https://doi.org/10.26640/22159045.279},
-  issn = {2215-9045},
-  journal = {Boletín Científico CIOH}
+title = {Modelamiento matemático de la dinámica marina en la región de Cartagena},
+author = {D.A. Uribe Suárez and Escobar Sierra, C. A. and Ruiz Sarrázola, D. A.},
+year = {2015},
+issue = {33},
+pages = {69-86},
+doi = {https://doi.org/10.26640/22159045.279},
+issn = {2215-9045},
+journal = {Boletín Científico CIOH}
 }
 
 @article{CIOH20152,
-  title = {Forzamientos marinos en fronteras abiertas de modelos costeros de pequeña escala, Cartagena},
-  author = {Ruiz Sarrázola, D. A. and Escobar Sierra, C. A. and D.A. Uribe Suárez},
-  year = {2015},
-  issue = {33},
-  pages = {53-68},
-  doi = {https://doi.org/10.26640/22159045.278},
-  issn = {2215-9045},
-  journal = {Boletín Científico CIOH}
+title = {Forzamientos marinos en fronteras abiertas de modelos costeros de pequeña escala, Cartagena},
+author = {Ruiz Sarrázola, D. A. and Escobar Sierra, C. A. and D.A. Uribe Suárez},
+year = {2015},
+issue = {33},
+pages = {53-68},
+doi = {https://doi.org/10.26640/22159045.278},
+issn = {2215-9045},
+journal = {Boletín Científico CIOH}
 }
 `;
 
 var TALKS_BIBTEX = `
 @inproceedings{PosterUTSA2017,
-  title = {A Multi-Scale Model to Investigate Nonlinear Dispersive Electrical Impulse Propagation along Cytoskeleton Filaments},
-  author = {Christian Hunley and D.A. Uribe Suárez and Marcelo Marucho},
-  booktitle = {College of Sciences Research Conference},
-  organization = {The University of Texas at San Antonio (UTSA)},
-  year = {2017},
-  address = {San Antonio, Texas, USA}
+title = {A Multi-Scale Model to Investigate Nonlinear Dispersive Electrical Impulse Propagation along Cytoskeleton Filaments},
+author = {Christian Hunley and D.A. Uribe Suárez and Marcelo Marucho},
+booktitle = {College of Sciences Research Conference},
+organization = {The University of Texas at San Antonio (UTSA)},
+year = {2017},
+address = {San Antonio, Texas, USA}
 }
 
 @inproceedings{ColloquePNP2018,
-  title = {Crack Propagation Criteria and Dynamic Insertion of Cohesive Elements Applied to Thermal Fatigue Failure of Comets/Asteroids},
-  author = {D.A. Uribe Suárez and Pierre-Olivier Bouchard and D. Pino-Muñoz and Marco Delbo},
-  booktitle = {Colloque Quadriennal de Bilan et Prospective Programme national de Planétologie},
-  organization = {Université Côte d'Azur and Observatoire de la Côte d'Azur},
-  year = {2018},
-  address = {Nice, France}
+title = {Crack Propagation Criteria and Dynamic Insertion of Cohesive Elements Applied to Thermal Fatigue Failure of Comets/Asteroids},
+author = {D.A. Uribe Suárez and Pierre-Olivier Bouchard and D. Pino-Muñoz and Marco Delbo},
+booktitle = {Colloque Quadriennal de Bilan et Prospective Programme national de Planétologie},
+organization = {Université Côte d'Azur and Observatoire de la Côte d'Azur},
+year = {2018},
+address = {Nice, France}
 }
 
 @inproceedings{SeminarioCIOH2014,
-  title = {Evaluation of Sources of Atmospheric Information in the Caribbean},
-  author = {D.A. Uribe Suárez and Escobar Sierra, C. A.},
-  booktitle = {Nuevos conocimientos y herramientas para el estudio de la erosión costera entre Galerazamba y Cartagena},
-  organization = {Centro de Investigaciones Oceanográficas e Hidrográficas del Caribe (CIOH)},
-  year = {2014},
-  address = {Cartagena de Indias, Bolivar, Colombia}
+title = {Evaluation of Sources of Atmospheric Information in the Caribbean},
+author = {D.A. Uribe Suárez and Escobar Sierra, C. A.},
+booktitle = {Nuevos conocimientos y herramientas para el estudio de la erosión costera entre Galerazamba y Cartagena},
+organization = {Centro de Investigaciones Oceanográficas e Hidrográficas del Caribe (CIOH)},
+year = {2014},
+address = {Cartagena de Indias, Bolivar, Colombia}
 }
 
 @inproceedings{CFRAC2019,
-  title = {Mesh-independent Crack Propagation under Mixed-Mode Loading using Remeshing and Dynamic Insertion of Cohesive Elements},
-  author = {D. Uribe and D. Pino-Muñoz and M. Delbo and P.-O. Bouchard},
-  booktitle = {VI International Conference on Computational Modeling of Fracture and Failure of Materials and Structures (CFRAC 2019)},
-  organization = {ECCOMAS Thematic Conference},
-  year = {2019},
-  address = {Braunschweig, Germany}
-}
-
-@inproceedings{MECAMAT2019,
-  title = {Towards a crack propagation criterion and dynamic insertion of cohesive elements applied to thermal fatigue failure of comets/asteroids},
-  author = {D.A. Uribe Suárez and Pierre-Olivier Bouchard and D. Pino-Muñoz and Marco Delbo},
-  booktitle = {Colloque National Mécamat-Rupture des Matériaux et des Structures},
-  organization = {L'association MECAMAT},
-  year = {2019},
-  address = {Aussois, France}
-}
-
-@inproceedings{ColloqueDoctorants2019,
-  title = {Mesh-independent Crack Propagation under Mixed-Mode Loading using Remeshing and Dynamic Insertion of Cohesive Elements},
-  author = {D. Uribe and D. Pino-Muñoz and Marco Delbo and Pierre-Olivier Bouchard},
-  booktitle = {Colloque des doctorants en 2ème année de l'EDSFA},
-  organization = {École doctorale Sciences Fondamentales et Appliquées},
-  year = {2019},
-  address = {Sophia Antipolis, France}
-}
-
-const POSTERS_BIBTEX = `
-@inproceedings{poster1,
-  author = {D. Uribe and D. Pino-Muñoz and M. Delbo and P.-O. Bouchard},
-  title = {Mesh-independent crack propagation under mixed-mode loading using remeshing and dynamic insertion of cohesive elements},
-  booktitle = {Colloque des doctorants en 2éme année de l’EDSFA},
-  address = {Sophia Antipolis, France},
-  year = {2019}
+title = {Mesh-independent Crack Propagation under Mixed-Mode Loading using Remeshing and Dynamic Insertion of Cohesive Elements},
+author = {D. Uribe and D. Pino-Muñoz and M. Delbo and P.-O. Bouchard},
+booktitle = {VI International Conference on Computational Modeling of Fracture and Failure of Materials and Structures (CFRAC 2019)},
+organization = {ECCOMAS Thematic Conference},
+year = {2019},
+address = {Braunschweig, Germany}
 }
 `;
 
-const REVIEWER_JOURNALS = [
-  "Icarus",
-  "Metallurgical Research and Technology (MRT)"
+var POSTERS_BIBTEX = `
+@inproceedings{ColloqueDoctorants2019,
+title = {Mesh-independent Crack Propagation under Mixed-Mode Loading using Remeshing and Dynamic Insertion of Cohesive Elements},
+author = {D. Uribe and D. Pino-Muñoz and Marco Delbo and Pierre-Olivier Bouchard},
+booktitle = {Colloque des doctorants en 2ème année de l'EDSFA},
+organization = {École doctorale Sciences Fondamentales et Appliquées},
+year = {2019},
+address = {Sophia Antipolis, France}
+}
+
+@inproceedings{MECAMAT2019,
+title = {Towards a crack propagation criterion and dynamic insertion of cohesive elements applied to thermal fatigue failure of comets/asteroids},
+author = {D.A. Uribe Suárez and Pierre-Olivier Bouchard and D. Pino-Muñoz and Marco Delbo},
+booktitle = {Colloque National Mécamat-Rupture des Matériaux et des Structures},
+organization = {L'association MECAMAT},
+year = {2019},
+address = {Aussois, France}
+}
+`;
+
+var REVIEWER_JOURNALS = [
+"Icarus",
+"Metallurgical Research and Technology (MRT)"
 ];
